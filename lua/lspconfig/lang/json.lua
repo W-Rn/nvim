1a57d2b9dc6ab9491d51becb7bef6ca6ce3bb9a5
W-Rn <502036389@qq.com>
2026-03-16 00:12:47 +0800

    json for mason delete

--- lua/lspconfig/lang/json.lua
+++ lua/lspconfig/lang/json.lua
@@ -12,9 +12,7 @@ return {
         opts = {
             ensure_installed = {
                 "json-lsp",
-                "prettierd",
                 "jq",
-                "fixjson",
             },
         },
     },
