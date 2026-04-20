bdd4ca7cdd623f6175d9b4f5b8bd6134500b4262
W-Rn <502036389@qq.com>
2026-04-20 11:52:53 +0800

    Disable the default case sensitivity behavior of key s, add line movement up/down in NVI mode for selected lines, and revise the mark logic.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -13,46 +13,49 @@ vim.keymap.set("n", "<C-H>", "<C-W><C-H>", { desc = "切换到左侧的分割窗
 vim.keymap.set("n", "<S-j>", "5j", { desc = "向下滚动 5 行" })
 vim.keymap.set("n", "<S-k>", "5k", { desc = "向上滚动 5 行" })
 
--- 禁用默认 键
-vim.keymap.set("n", "s", "<nop>")
+-- 禁用默认键
+vim.keymap.set({ "n", "v" }, "s", "<nop>")
+vim.keymap.set({ "n", "v" }, "S", "<nop>")
 vim.keymap.set({ "n", "x" }, "gra", "<nop>")
 vim.keymap.set("n", "grn", "<nop>")
 vim.keymap.set("n", "gri", "<nop>")
 vim.keymap.set("n", "grr", "<nop>")
+vim.keymap.set("n", "grt", "<nop>")
 vim.keymap.set("n", "gd", "<nop>")
+vim.keymap.set("n", "gO", "<nop>")
 
--- -- 窗口大小调整
+-- 窗口大小调整
 vim.keymap.set("n", "<Up>", ":res +1<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<Down>", ":res -1<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<Left>", ":vertical resize +1<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<Right>", ":vertical resize -1<CR>", { noremap = true, silent = true })
 
 -- mark
-for c = string.byte "a", string.byte "z" do
-    local key = string.char(c)
-    vim.keymap.set("n", "m" .. key, function()
-        return "m" .. key .. "<Cmd>redrawstatus<CR>"
-    end, { expr = true, noremap = true })
+local mark_chars = {}
+for i = string.byte "a", string.byte "z" do
+    table.insert(mark_chars, string.char(i))
+end
+for i = string.byte "A", string.byte "Z" do
+    table.insert(mark_chars, string.char(i))
 end
 
-for c = string.byte "A", string.byte "Z" do
-    local key = string.char(c)
+for _, key in ipairs(mark_chars) do
     vim.keymap.set("n", "m" .. key, function()
-        return "m" .. key .. "<Cmd>redrawstatus<CR>"
-    end, { expr = true, noremap = true })
-end
+        return "m" .. key .. "<Cmd>redraw!<CR>"
+    end, { expr = true, desc = "Set Mark" .. key })
 
-for c = string.byte "a", string.byte "z" do
-    local key = string.char(c)
     vim.keymap.set("n", "dm" .. key, function()
         vim.cmd("delmarks " .. key)
         vim.cmd "redraw!"
-    end, { noremap = true })
+    end, { silent = true, desc = "Remove Mark" .. key })
 end
-for c = string.byte "A", string.byte "Z" do
-    local key = string.char(c)
-    vim.keymap.set("n", "dm" .. key, function()
-        vim.cmd("delmarks " .. key)
-        vim.cmd "redraw!"
-    end, { noremap = true })
-end
+
+-- ==========================================
+-- 行/块 移动快捷键 (Alt + s/w)
+-- ==========================================
+vim.keymap.set("n", "<A-s>", "<Cmd>m .+1<CR>==", { desc = "向下移动当前行" })
+vim.keymap.set("n", "<A-w>", "<Cmd>m .-2<CR>==", { desc = "向上移动当前行" })
+vim.keymap.set("v", "<A-s>", ":m '>+1<CR>gv=gv", { desc = "向下移动选中块", silent = true })
+vim.keymap.set("v", "<A-w>", ":m '<-2<CR>gv=gv", { desc = "向上移动选中块", silent = true })
+vim.keymap.set("i", "<A-s>", "<Esc><Cmd>m .+1<CR>==gi", { desc = "向下移动当前行" })
+vim.keymap.set("i", "<A-w>", "<Esc><Cmd>m .-2<CR>==gi", { desc = "向上移动当前行" })
