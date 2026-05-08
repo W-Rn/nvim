9269ff1520538412c7e787b75284a2effd7eb33c
W-Rn <502036389@qq.com>
2026-05-09 01:55:27 +0800

    close_with_q add nvim-pack

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -24,6 +24,7 @@ vim.api.nvim_create_autocmd("FileType", {
     group = vim.api.nvim_create_augroup("close_with_q", { clear = true }),
     pattern = {
         "checkhealth",
+        "nvim-pack",
         "help",
         "qf",
     },
