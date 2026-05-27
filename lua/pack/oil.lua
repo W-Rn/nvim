5c13919b5f51417103d65d8a33bf57e9b40bf1f5
W-Rn <502036389@qq.com>
2026-05-27 23:51:06 +0800

    oil左侧图标列显示git状态

--- lua/pack/oil.lua
+++ lua/pack/oil.lua
@@ -1,6 +1,6 @@
 vim.pack.add({
     { src = "https://github.com/stevearc/oil.nvim" },
-    { src = "https://github.com/benomahony/oil-git.nvim" },
+    { src = "https://github.com/W-Rn/oil-git-signs.nvim" },
 }, { load = function() end, confirm = false })
 
 function _G.get_oil_winbar()
@@ -20,7 +20,7 @@ local function ensure_oil()
     local detail = false
     oil_loaded = true
     vim.cmd.packadd("oil.nvim")
-    vim.cmd.packadd("oil-git.nvim")
+    vim.cmd.packadd("oil-git-signs.nvim")
     require("oil").setup({
         default_file_explorer = true,
         keymaps = {
@@ -53,6 +53,7 @@ local function ensure_oil()
         use_default_keymaps = false,
         win_options = {
             winbar = "%!v:lua.get_oil_winbar()",
+            signcolumn = "yes:2",
         },
         float = {
             border = "rounded",
